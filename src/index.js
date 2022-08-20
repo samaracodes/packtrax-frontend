1feac0617585afcf2130680980f126bf80d1bfa0
Sam <samara.amanda01@gmail.com>
2022-08-20 14:20:20 -0400

    implemented redux store, installed react packages

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import thunk from 'redux-thunk'
+
+const reducer = combineReducers ({
+  
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(
+  reducer, 
+  composeEnhancer(applyMiddleware(thunk))
+)
+
+const container = document.getElementById('root');
+
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
